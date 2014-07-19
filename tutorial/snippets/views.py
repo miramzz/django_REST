9acfac2d82313b64a19b638c6bdcb83ccdcd40cb
miramzz <mira.muazzez@gmail.com>
2014-07-18 23:01:04 -0700

    typos corrected

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,4 +1,4 @@
-ffrom rest_framework import status
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from snippets.models import Snippet
