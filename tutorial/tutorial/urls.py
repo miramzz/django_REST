55afaf1d3c4a190f523891168e6696e4f6c586bb
miramzz <mira.muazzez@gmail.com>
2014-07-19 14:35:07 -0700

    urls fixed - wrong implementation

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -1,12 +1,8 @@
 from django.conf.urls import patterns, url
-from snippets import views
 from django.conf.urls import include
 
-
 urlpatterns = patterns('',
-    url(r'^users/$', views.UserList.as_view()),
-    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
-
+    url(r'^', include('snippets.urls')),
 )
 
 urlpatterns += patterns('',
