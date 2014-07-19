77bae2e22c3ca99db88a3159784c06e4357ec9fa
miramzz <mira.muazzez@gmail.com>
2014-07-18 23:54:10 -0700

    class based views refactoring

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url
+from snippets import views
 
-urlpatterns = patterns('snippets.views',
-    url(r'^snippets/$', 'snippet_list'),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', 'snippet_detail'),
+urlpatterns = patterns('',
+    url(r'^snippets/$', views.SnippetList.as_view()),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
 )
