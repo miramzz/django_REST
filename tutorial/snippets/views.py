2a557fb88827e0509c7ba2e0b61371559600c7df
miramzz <mira.muazzez@gmail.com>
2014-07-19 14:13:28 -0700

    urls updated for user login

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,6 +1,7 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 from snippets.serializers import UserSerializer
+from rest_framework import permissions
 from rest_framework import generics
 from django.contrib.auth.models import User
 
@@ -8,6 +9,7 @@ from django.contrib.auth.models import User
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     def pre_save(self, obj):
         obj.owner = self.request.user
 
@@ -15,6 +17,7 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     def pre_save(self, obj):
         obj.owner = self.request.user
 
