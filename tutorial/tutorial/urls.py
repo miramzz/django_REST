b5ec1098c6a8a130f47ca1fb7566182ce3cf945d
miramzz <mira.muazzez@gmail.com>
2014-07-19 14:17:59 -0700

    login added

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -2,6 +2,13 @@ from django.conf.urls import patterns, url
 from snippets import views
 from django.conf.urls import include
 
+
+urlpatterns = patterns('',
+    url(r'^users/$', views.UserList.as_view()),
+    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
+
+)
+
 urlpatterns += patterns('',
     url(r'^api-auth/', include('rest_framework.urls',
                                namespace='rest_framework')),
